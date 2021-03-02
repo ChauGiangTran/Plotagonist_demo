c47c3ffef2dca1f17caa73619cf75131d9d8cff0
tranle15788 <47651173+tranle15788@users.noreply.github.com>
2021-03-02 15:20:40 +0700

    at

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -10,4 +10,10 @@ class Data {
 class Colors {
   Colors._(); // this basically makes it so you can instantiate this class
   static const Color blue = Color(0xff007AFF);
+  static const Color grey = Color(0xff979797);
+  static const Color grey100 = Color(0xffE4E4E4);
+  static const Color grey200 = Color(0xff4C4C4C);
+  static const Color orange = Color(0xffED8A19);
+  static const Color orange100 = Color(0xffE5DBCF);
+  static const Color white100 = Color(0xffE7E7E7);
 }
